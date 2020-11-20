32405ffca921c5c609db6f4507bfd9742fe8670d
RadAzzouz <rad@pspdfkit.com>
2020-11-20 15:27:26 -0500

    Set the license key in the example app

--- Example/PDFXKitExample/AppController.swift
+++ Example/PDFXKitExample/AppController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import PSPDFKit
 
 @UIApplicationMain
 class AppController: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        // Set your license key here. PSPDFKit is commercial software.
+        // Each PSPDFKit license is bound to a specific app bundle id.
+        // Visit https://customers.pspdfkit.com to get your demo or commercial license key.
+        SDK.setLicenseKey("YOUR_LICENSE_KEY_GOES_HERE")
+        return true;
+    }
 }
