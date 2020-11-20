617e79cb25ba4a85aefe85b8c8470248259c2ad9
RadAzzouz <rad@pspdfkit.com>
2020-11-20 15:59:39 -0500

    Fix the example project on Mac Catalyst

--- Example/PDFXKitExample/AppController.swift
+++ Example/PDFXKitExample/AppController.swift
@@ -13,8 +13,7 @@ import PSPDFKit
 class AppController: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
+    public func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // Set your license key here. PSPDFKit is commercial software.
         // Each PSPDFKit license is bound to a specific app bundle id.
         // Visit https://customers.pspdfkit.com to get your demo or commercial license key.
