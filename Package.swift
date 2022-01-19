6f95feb11e14f147e1682690ea27e1025d9aaf61
Daniel Langh <daniel.langh@gmail.com>
2022-01-19 16:26:08 +0100

    added package.swift

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "PDFXKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "PDFXKit",
+            targets: ["PDFXKitObjC", "PDFXKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/PSPDFKit/PSPDFKit-SP", from: "11.2.0"),
+    ],
+    targets: [
+        .target(
+            name: "PDFXKitObjC",
+            dependencies: [
+                .product(name: "PSPDFKit", package: "PSPDFKit-SP")
+            ],
+            path: "Sources",
+            exclude: ["Swift"],
+			publicHeadersPath: ""),
+        .target(
+            name: "PDFXKit",
+            dependencies: ["PDFXKitObjC"],
+            path: "Sources/Swift"),
+        .testTarget(
+            name: "PDFXKitTests",
+            dependencies: ["PDFXKit"],
+            path: "Tests")
+    ]
+)
