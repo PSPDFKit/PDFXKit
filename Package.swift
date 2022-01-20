53dfa3b08e3f9cff0f7b7cd97b40e59f55e351c7
Daniel Langh <daniel.langh@gmail.com>
2022-01-20 13:52:06 +0100

    raised deplyoment targets to ios13

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "PDFXKit",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "PDFXKit",
